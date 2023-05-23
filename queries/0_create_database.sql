01d54ac468fe6163ec1d441d3fc081f8570933f2
ggors <ggorski@consoneo.com>
2023-05-23 14:36:02 +0200

    ✨ Create Tables & Insert Company

--- queries/0_create_database.sql
+++ queries/0_create_database.sql
@@ -1,3 +1,41 @@
 # Suit les instruction du checkpoint, en utilisant https://www.dbdesigner.net/, créer un script
-# d'initialisation de BDD SQLite et copie colle son contenu ici
-# (tu dois notmalement avoir +/- 4 instructions CREATE TABLE, chaque instructions doit bien se terminer par un ";")
+# d'initialisation deavoir BDD SQLite et copie colle son contenu ici
+# (tu dois notmalement  +/- 4 instructions CREATE TABLE, chaque instructions doit bien se terminer par un ";")
+
+CREATE TABLE Recruiter
+(
+    id         integer PRIMARY KEY AUTOINCREMENT,
+    login      string,
+    password   string,
+    company_id integer
+);
+
+CREATE TABLE Candidate
+(
+    id           integer PRIMARY KEY AUTOINCREMENT,
+    login        string,
+    password     string,
+    name         string,
+    firstname    string,
+    email        string,
+    phone        integer,
+    presentation text,
+    job_offer_id integer
+);
+
+CREATE TABLE Company
+(
+    id           integer PRIMARY KEY AUTOINCREMENT,
+    recruiter_id integer,
+    name         string,
+    description text
+);
+
+CREATE TABLE Job_offer
+(
+    id           integer PRIMARY KEY AUTOINCREMENT,
+    title        string,
+    description text,
+    city         string,
+    candidate_id integer
+);
--- queries/1_insert_data.sql
+++ queries/1_insert_data.sql
@@ -1,3 +1,9 @@
 # Écris dans ce fichier quelques instructions SQL pour insérer de données dans ta BDD
 # N'oublie pas, les requêtes SQL doivent se terminer avec un ";"
 # Pense à céer au moins une entreprise nommée "WCS", à créer des offre d'emploi à "Paris" dont une qui s'intitule "Dev"
+
+INSERT INTO Company (name, description) VALUES
+ ('PROBTP', 'Assurance'),
+ ('Crédit Agricole', 'Banque');
+
+SELECT * FROM Company;
